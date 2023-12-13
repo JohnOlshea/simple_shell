b1c724f33f085f6fafaa6f9680b75424165def6b
John <johnolshea@gmail.com>
2023-12-13 03:42:54 -0800

    fixed betty lint complaint

--- main.c
+++ main.c
@@ -44,50 +44,49 @@ int print_string(char *str)
  */
 void execute_command(char *args[])
 {
-    pid_t pid = fork();
+	int status;
+	pid_t pid = fork();
 
-    if (pid < 0)
-    {
-        perror("fork");
-        exit(EXIT_FAILURE);
-    }
-    else if (pid == 0)
-    {
-        if (execve(args[0], args, NULL) == -1)
+	if (pid < 0)
 	{
-            /*perror("execve");*/
-            exit(EXIT_FAILURE);
-        }
-    } else
-    {
-        int status;
-        if (wait(&status) == -1)
+		perror("fork");
+		exit(EXIT_FAILURE);
+	}
+	else if (pid == 0)
 	{
-            exit(EXIT_FAILURE);
-        }
-    }
+		if (execve(args[0], args, NULL) == -1)
+		{
+			perror("execve");
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		if (wait(&status) == -1)
+			exit(EXIT_FAILURE);
+	}
 }
 
 /**
  * tokenize_input - x
- * @input - x
- * @args - x
+ * @input: x
+ * @args: x
  *
  * Return: Always 0.
  */
 void tokenize_input(char *input, char *args[])
 {
-    int arg_count = 0;
-    char *token = strtok(input, " \n");
+	int arg_count = 0;
+	char *token = strtok(input, " \n");
 
-    while (token != NULL && arg_count < MAX_INPUT_SIZE - 1)
-    {
-        args[arg_count] = token;
-        arg_count++;
-        token = strtok(NULL, " \n");
-    }
+	while (token != NULL && arg_count < MAX_INPUT_SIZE - 1)
+	{
+		args[arg_count] = token;
+		arg_count++;
+		token = strtok(NULL, " \n");
+	}
 
-    args[arg_count] = NULL;
+	args[arg_count] = NULL;
 }
 
 /**
@@ -97,33 +96,33 @@ void tokenize_input(char *input, char *args[])
  */
 int main(void)
 {
-    char *input = NULL;
-    size_t input_size = 0;
-    ssize_t chars_read;
-    char *args[MAX_INPUT_SIZE];
+	char *input = NULL;
+	size_t input_size = 0;
+	ssize_t chars_read;
+	char *args[MAX_INPUT_SIZE];
 
-    while (1)
-    {
-        if (isatty(0))
-            print_string("$ ");
-
-        chars_read = getline(&input, &input_size, stdin);
-        if (chars_read < 0)
+	while (1)
 	{
-            if (isatty(0))
-                print_string("\n");
-            free(input);
-            exit(EXIT_FAILURE);
-        }
+		if (isatty(0))
+		print_string("$ ");
 
-        if (input[chars_read - 1] == '\n') 
-            input[chars_read - 1] = '\0';
+		chars_read = getline(&input, &input_size, stdin);
+		if (chars_read < 0)
+		{
+			if (isatty(0))
+			print_string("\n");
+			free(input);
+			exit(EXIT_FAILURE);
+		}
 
-        tokenize_input(input, args);
+		if (input[chars_read - 1] == '\n')
+			input[chars_read - 1] = '\0';
 
-        if (args[0] != NULL)
-            execute_command(args);
-    }
+		tokenize_input(input, args);
 
-    return (0);
+		if (args[0] != NULL)
+			execute_command(args);
+	}
+
+	return (0);
 }
