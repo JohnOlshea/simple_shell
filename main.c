4f00a0d273e15e2873be56105e99e52dcfa7cc7d
John <johnolshea@gmail.com>
2023-12-13 07:13:37 -0800

    add search path to execute command

--- main.c
+++ main.c
@@ -38,11 +38,133 @@ int main(void)
 		tokenize_input(input, args);
 
 		if (args[0] != NULL)
+		{
 			execute_command(args);
+		}
 	}
 
 	free(input);
 	return (0);
+}
+
+/**
+ * execute_with_fork - Execute a command with fork, execve, and wait
+ * @command: x
+ * @args: x
+ *
+ */
+void execute_with_fork(char *command, char *args[])
+{
+	int status;
+	pid_t pid = fork();
+
+	if (pid < 0)
+	{
+		perror("fork");
+		exit(EXIT_FAILURE);
+	}
+	else if (pid == 0)
+	{
+		if (execve(command, args, environ) == -1)
+		{
+			if (!isatty(0))
+			{
+				print_string("./hsh: 1: ");
+				print_string(command);
+				print_string(": not found\n");
+				exit(127);
+			}
+			perror(command);
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		if (wait(&status) == -1)
+			exit(EXIT_FAILURE);
+	}
+}
+
+/**
+ * execute_in_path - Search for the command in PATH and execute if found
+ *
+ * @command: command to execute
+ * @args: array or argument and flags
+ */
+void execute_in_path(char *command, char *args[])
+{
+	char *path_env = getenv("PATH");
+	char full_path[MAX_INPUT_SIZE];
+	char *path_copy;
+	char *token;
+
+	if (path_env == NULL)
+	{
+		print_string(command);
+		print_string(": ");
+		print_string(args[0]);
+		print_string(": not found\n");
+		exit(EXIT_FAILURE);
+	}
+
+	path_copy = strdup(path_env);
+	if (path_copy == NULL)
+	{
+		perror(command);
+		exit(EXIT_FAILURE);
+	}
+
+	token = strtok(path_copy, ":");
+	while (token != NULL)
+	{
+		strcpy(full_path, token);
+		strcat(full_path, "/");
+		strcat(full_path, command);
+
+		if (access(full_path, X_OK) == 0)
+		{
+			execute_with_fork(full_path, args);
+			free(path_copy);
+			return;
+		}
+
+		token = strtok(NULL, ":");
+	}
+
+	print_string(command);
+	print_string(": ");
+	print_string(args[0]);
+	print_string(": not found\n");
+	free(path_copy);
+	exit(EXIT_FAILURE);
+}
+
+/**
+ * execute_directly - Execute a command directly using its full path
+ * @command: command to execute
+ * @args: array or argument and flags
+ *
+ */
+void execute_directly(char *command, char *args[])
+{
+	execute_with_fork(command, args);
+}
+
+/**
+ * execute_command - Execute a command, either directly or by searching in PATH
+ * @args: array or argument and flags
+ *
+ */
+void execute_command(char *args[])
+{
+	if (args[0][0] == '/' || args[0][0] == '.')
+	{
+		execute_directly(args[0], args);
+	}
+	else
+	{
+		execute_in_path(args[0], args);
+	}
 }
 
 /**
@@ -75,43 +197,6 @@ int print_string(char *str)
 	return (count);
 }
 
-/**
- * execute_command - x
- * @args: x
- *
- */
-void execute_command(char *args[])
-{
-	int status;
-	pid_t pid = fork();
-
-	if (pid < 0)
-	{
-		perror("fork");
-		exit(EXIT_FAILURE);
-	}
-	else if (pid == 0)
-	{
-		if (execve(args[0], args, environ) == -1)
-		{
-			if (!isatty(0))
-			{
-				print_string("./hsh: 1: ");
-				print_string(args[0]);
-				print_string(": not found\n");
-				exit(127);
-			}
-
-			perror(args[0]);
-			exit(EXIT_FAILURE);
-		}
-	}
-	else
-	{
-		if (wait(&status) == -1)
-			exit(EXIT_FAILURE);
-	}
-}
 
 /**
  * tokenize_input - x
