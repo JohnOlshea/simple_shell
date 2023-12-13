43a0c2fea60e88310dcbf09467a2d81654e6bdeb
John <johnolshea@gmail.com>
2023-12-12 22:10:21 -0800

    add getline

--- main.c
+++ main.c
@@ -1,4 +1,10 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 /**
  * main - Simple shell
@@ -7,7 +13,54 @@
  */
 int main(void)
 {
-	printf("($) ");
+	char *input = NULL;
+	size_t input_size = 0;
+	char *newline;
+	char *args[MAX_INPUT_SIZE];
+	int f_i;
+
+	while (1)
+	{
+		if (isatty(0))
+		{
+			write(STDOUT_FILENO, "($) ", 4);
+			fflush(stdout);
+		}
+
+		if (getline(&input, &input_size, stdin) == -1)
+		{
+			if (isatty(0))
+				write(STDOUT_FILENO, "\n", 1);
+			perror("getline");
+			exit(EXIT_FAILURE);
+		}
+
+		/* Remove trailing newline */
+		newline = strchr(input, '\n');
+
+		if (newline != NULL)
+		{
+			*newline = '\0';
+		}
+
+		/* Ignore comments (lines starting with '#') */
+
+		/* Tokenize the input */
+
+		/* Check for the exit built-in command */
+
+		/* Check for the env built-in command */
+
+		printf("Shelly");
+
+		/* Free memory for tokens */
+		for (f_i = 0; args[f_i] != NULL; f_i++)
+		{
+			free(args[f_i]);
+		}
+	}
+
+	free(input);
 
 	return (0);
 }
